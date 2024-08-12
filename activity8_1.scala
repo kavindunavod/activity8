e70185159b7c200570727bd61d4bdf0c479e7f72
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-08-12 23:20:35 +0530

    Update activity8_1.scala

--- activity8_1.scala
+++ activity8_1.scala
@@ -1,10 +1,9 @@
-object activity8_1 {
-
+object Activity8_1 {
 
   def encrypt(text: String, shift: Int): String = {
     text.map { char =>
       if (char.isLetter) {
-        val shiftAmount = shift % 26
+        val shiftAmount = (shift % 26 + 26) % 26 // Normalize shift to be between 0 and 25
         val base = if (char.isUpper) 'A' else 'a'
         ((char - base + shiftAmount) % 26 + base).toChar
       } else {
@@ -13,11 +12,10 @@ object activity8_1 {
     }.mkString
   }
 
-
   def decrypt(text: String, shift: Int): String = {
     text.map { char =>
       if (char.isLetter) {
-        val shiftAmount = shift % 26
+        val shiftAmount = (shift % 26 + 26) % 26 // Normalize shift to be between 0 and 25
         val base = if (char.isUpper) 'A' else 'a'
         ((char - base - shiftAmount + 26) % 26 + base).toChar
       } else {
@@ -26,15 +24,13 @@ object activity8_1 {
     }.mkString
   }
 
-    def cipher(text: String, shift: Int, algorithm: (String, Int) => String): String = {
+  def cipher(text: String, shift: Int, algorithm: (String, Int) => String): String = {
     algorithm(text, shift)
   }
 
-
   def main(args: Array[String]): Unit = {
-    val plaintext = "Hello, World!"
+    val plaintext = "Hello, World! I am Kavindu"
     val shift = 3
-
 
     val encrypted = cipher(plaintext, shift, encrypt)
     println(s"Encrypted: $encrypted")
